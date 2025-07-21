1cfd6f4020d835884bd39ad2ab2a840593fb97f0
lambergabr <lambertegab1210@gmail.com>
2025-07-22 10:16:35 +1200

    Copying Previous Code
    First Code. Not my final.

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -17,9 +17,77 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(255, 233, 182); //light honeydew green colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
   rect(40 ,40, rect_width, rect_height);
+
+DrawGatt();
+
+}
+
+function DrawGatt(){
+
+  var leftearX = 30;
+  var leftearY = 20
+
+  var rightearX = 65;
+  var rightearY = 85;
+
+  strokeWeight(3);
+  stroke(0);
+  fill(255);
+
+   // Start drawing the shape.
+beginShape();
+
+  // Add vertices.
+
+  //left ear
+  vertex(leftearX, leftearY);//tip of left ear // (30,20) before
+  vertex(leftearX +20, leftearY+20); //left head
+
+  //right ear
+  vertex(rightearX, 40); //right head
+  vertex(rightearX+20, rightearY-65);//tip of right ear
+
+  //right hand 
+  vertex(rightearX+20, rightearY-35); //armpit
+  vertex(rightearX+32, rightearY-20); // hand
+  vertex(rightearX+20,rightearY-25); //shoulder
+
+  //right foot
+  vertex(rightearX+20, rightearY+15); //right foot
+  vertex(rightearX,rightearY); //right crotch
+
+  //left foot
+  vertex(leftearX +20 ,leftearY+65); // crotch
+  vertex(leftearX, leftearY+80); // foot
+
+  //tail
+  vertex(leftearX, leftearY+70);
+  vertex(leftearX -10, leftearY+70);
+  vertex(leftearX, leftearY+65)
+
+  //left hand
+  vertex(leftearX, leftearY+30); //arm pit
+  vertex(leftearX-13,leftearY+45); // hand
+  vertex(leftearX, leftearY+40); //shoulder
+
+  // Stop drawing the shape.
+endShape(CLOSE);
+
+//eyes
+  stroke(0)
+  strokeWeight(1);
+  fill(255);
+  ellipse(leftearX +15, leftearY +30, 3, 3); //left eye
+  ellipse(rightearX+5, rightearY -35, 3,3); //right eye
+
+//mouth
+ strokeWeight(1);
+ stroke(0);
+  line(leftearX+20, leftearY +30, rightearX, rightearY-35);
+
 }
